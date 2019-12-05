233c34898bc7628482fd97b3d8d580d75ee94343
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2019-12-05 15:46:08 -0600

    Update auth.js

--- 1.0.0/auth.js
+++ 1.0.0/auth.js
@@ -1,29 +1,6 @@
 /*
 Code created by Junhao Zhang @coder100: repl.it/@coder100
 */
-/*
-var frame = document.createElement('iframe');
-//frame.src = 'http://repl.it/auth_with_repl_site?domain=' + location.host;
-frame.src = 'https://repl.it/auth_with_repl_site?domain=' + location.host;
-frame.width = "100%";
-frame.frameBorder = '0';
-frame.style.minHeight = '400px';
-frame.style.boxShadow = '3px 3px 10px rgba(0, 0, 0, 0.2)';
-frame.style.border = 'solid 1px #888';
-frame.style.borderRadius = '5px';
-
-var selem = document.currentScript;
-
-window.addEventListener("message", function(e) {
-	console.log('message');
-	if (!selem.attributes.authed) return;
-	if (e.data !== 'auth_complete') return;
-
-	eval(selem.attributes.authed.value);
-}, false);
-
-selem.parentNode.insertBefore(frame, selem);
-*/
 let auth = function(){
   if(arguments.length==0){
     this.cls = false;
