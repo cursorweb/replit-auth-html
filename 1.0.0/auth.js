e4db1a21e60cfe380987f21f7cd4f59a3806303c
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2019-12-05 15:45:57 -0600

    Update auth.js

--- 1.0.0/auth.js
+++ 1.0.0/auth.js
@@ -26,24 +26,43 @@ selem.parentNode.insertBefore(frame, selem);
 */
 let auth = function(){
   if(arguments.length==0){
-    this.cls = "";
+    this.cls = false;
     this.styling = {
       minHeight:"400px",
       boxShadow:"3px 3px 10px rgba(0, 0, 0, 0.2)",
       border:"solid 1px #888",
       borderRadius = "5px"
     }
-    
+    this.ondone = "location.reload()";
   }elseif(arguments.length==1){
     this.cls = arguments[0]
+    this.styling = {
+      minHeight:"400px",
+      boxShadow:"3px 3px 10px rgba(0, 0, 0, 0.2)",
+      border:"solid 1px #888",
+      borderRadius = "5px"
+    }
+    this.ondone = "location.reload()"
   }elseif(arguments.length==2){
     this.cls = arguments[0]
     this.styling = arguments[1]
+    this.ondone = "location.reload()"
   }
-  else{
+  elseif(arguments.length==3){
+    this.cls = arguments[0]
+    this.styling = arguments[1]
+    this.ondone = arguments[2]
+  }else{
     console.error("REPL.IT AUTH ERROR: Too many arguments put in.")
   }
 }
-auth.prototype.append = function(elem, before){
-  //elem.
+auth.prototype.append = function(elem){
+  var element = document.createElement('iframe')
+  element.src = 'https://repl.it/auth_with_repl_site?domain=' + location.host
+  element.style = this.styling
+  if(this.cls) element.cls = this.cls
+  elem.parentNode.insertBefore(element, elem)
+  window.addEventListener("message", function(e) {
+    eval(this.ondone)
+  }, false)
 }
